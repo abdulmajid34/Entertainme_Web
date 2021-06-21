f9c49cbd8ec6563577e8eed3bad3c0318034f59f
abdulmajid <abdulmajid08180881@gmail.com>
2021-06-21 22:25:04 +0700

    added server movies

--- server/services/movies/app.js
+++ server/services/movies/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 4001
 const cors = require('cors')
-const connect = require('./config/mongodb')
+const { connect } = require('./config/mongodb')
 const routes = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
 
